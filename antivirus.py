4d0f44a118cd9ba6518b4ef8fb5007dd3d887e26
cameronmcnz <ita930174@gmail.com>
2023-11-25 22:05:06 +0200

    update

--- antivirus.py
+++ antivirus.py
@@ -2,6 +2,107 @@ import os
 import requests
 import tkinter as tk
 from tkinter import ttk, filedialog
+import threading
+
+class ScanThread(threading.Thread):
+    def __init__(self, files, api_key, update_callback, error_callback, complete_callback):
+        super().__init__()
+        self.files = files
+        self.api_key = api_key
+        self.update_callback = update_callback
+        self.error_callback = error_callback
+        self.complete_callback = complete_callback
+
+    def is_valid_api_key(self, file):
+        # Test request to a non-authenticated VirusTotal API endpoint
+        try:
+            url = "https://www.virustotal.com/api/v3/files"
+            files = {"file": (file, open(file, "rb"), "text/plain")}
+            headers = {
+                "accept": "application/json",
+                "x-apikey": self.api_key
+            }
+
+            response = requests.post(url, files=files, headers=headers)
+            response.raise_for_status()  # Raise an exception if the response status code is not 200
+            return True
+        except requests.exceptions.HTTPError as e:
+            # Handle HTTP errors and display a message to the user
+            self.error_callback(f"HTTP Error: {e}")
+            return False
+        except Exception as e:
+            # Handle other errors and display a message to the user
+            self.error_callback(f"Error: {e}")
+            return False
+
+    def run(self):
+        try:
+            for file in self.files:
+                if self.is_valid_api_key(file):
+                    result = self.check_file(file)
+                    self.update_callback(result, file)
+                else:
+                    self.error_callback("Invalid VirusTotal API key.")
+                    return
+            self.complete_callback("Scan completed.")
+        except Exception as e:
+            self.error_callback(f"Unexpected error: {str(e)}")
+
+    def Scan_id(self, file):
+        url = "https://www.virustotal.com/api/v3/files"
+        files = {"file": (file, open(file, "rb"), "text/plain")}
+        headers = {
+            "accept": "application/json",
+            "x-apikey": self.api_key
+        }
+
+        response = requests.post(url, files=files, headers=headers)
+
+        result = response.json()
+        scan_id = result.get('data', {}).get('id')
+        if scan_id:
+            return scan_id
+        else:
+            raise ValueError("Failed to retrieve scan ID from the response.")
+        
+    def check_file(self, file_path):
+        try:
+            Id = self.Scan_id(file_path)
+            url = f"https://www.virustotal.com/api/v3/analyses/{Id}"
+            headers = {
+                "accept": "application/json",
+                "x-apikey": self.api_key
+            }
+            response = requests.get(url, headers=headers)
+
+            # Process the response to check if the file is malicious
+            result = response.json()
+
+            data = result["data"]
+
+            attributes = data["attributes"]
+
+            stats = attributes["stats"]
+
+            malicious_count = stats["malicious"]
+
+            if malicious_count > 0:
+                return "Malicious"
+            else:
+                return "Clean"
+        except requests.exceptions.HTTPError as err:
+            print(f"HTTP Error: {err}")
+            return "Error"
+        except requests.exceptions.RequestException as err:
+            print(f"Request Exception: {err}")
+            return "Error"
+        except Exception as err:
+            print(f"Unexpected error occurred: {err}")
+            return "Error"
+
+
+
+
 
 class DirectoryScanner:
     def __init__(self, master):
@@ -13,6 +114,7 @@ class DirectoryScanner:
         # Variables to store folder, API key, and scan status
         self.folder_var = tk.StringVar()
         self.api_key_var = tk.StringVar()
+        self.scan_response_var = tk.StringVar()
         self.scan_status_var = tk.StringVar()
 
         # Create a menu bar
@@ -39,16 +141,20 @@ class DirectoryScanner:
         self.folder_count_label.grid(row=2, column=0, pady=5, padx=10, sticky=tk.W)
 
         # Label and entry for entering VirusTotal API key
-        ttk.Label(self.frame, text="VirusTotal API Key:").grid(row=5, column=0, pady=5, sticky=tk.W)
-        self.api_key_entry = ttk.Entry(self.frame, textvariable=self.api_key_var, show="*")
-        self.api_key_entry.grid(row=6, column=0, pady=10, padx=10, sticky=tk.W)
+        # ttk.Label(self.frame, text="VirusTotal API Key:").grid(row=5, column=0, pady=5, sticky=tk.W)
+        # self.api_key_entry = ttk.Entry(self.frame, textvariable=self.api_key_var)
+        # self.api_key_entry.grid(row=6, column=0, pady=10, padx=10, sticky=tk.W)
+        self.api_key_var.set("16fe085c26136d98480f1c18f0d22b2e42c9ab81b500cd5be2477ec82ce69a47")
 
         # Button to start the scan
         ttk.Button(self.frame, text="Scan Directory", command=self.scan_directory).grid(row=7, column=0, pady=10, sticky=tk.W)
 
         # Label to display the scan status
         self.status_label = ttk.Label(self.frame, textvariable=self.scan_status_var)
-        self.status_label.grid(row=8, column=0, pady=10, sticky=tk.W)
+        self.status_label.grid(row=9, column=0, pady=2, sticky=tk.W)
+        # Label to display the scan response
+        self.status_label = ttk.Label(self.frame, textvariable=self.scan_response_var)
+        self.status_label.grid(row=8, column=0, pady=2, sticky=tk.W)
 
     def select_directory(self):
         # Prompt the user to select a directory
@@ -77,109 +183,33 @@ class DirectoryScanner:
             self.folder_count_label.config(text=count_text)
 
     def scan_directory(self):
-        # Get the API key from the entry widget
         api_key = self.api_key_var.get()
-
-         # Get the selected directory
         directory = self.folder_var.get()
 
-        # Check if both directory and API key are empty
         if not directory and not api_key:
             self.scan_status_var.set("Please select a folder and enter your VirusTotal API key.")
             return
-
-        # Check if only the API key is empty
         elif not api_key:
             self.scan_status_var.set("Please enter a VirusTotal API key.")
             return
-
-        # Check if the directory is empty
         elif not directory:
             self.scan_status_var.set("Please select a folder.")
             return
 
-        # Get the files and subdirectories in the selected directory
         files, _ = self.get_files_and_subdirectories(directory)
-    
-        # Scan each file in the directory
-        for file in files:
-            # Check if the provided API key is valid
-            if self.is_valid_api_key(file,api_key):
-                result = self.check_file(file, api_key)
-                status_text = f'{os.path.basename(file)}: {result}'
-                print(status_text)
-                self.scan_status_var.set(status_text)
-            else:
-                self.scan_status_var.set("Invalid VirusTotal API key.")
-                return
 
-    def is_valid_api_key(self, file ,api_key):
-        # Test request to a non-authenticated VirusTotal API endpoint
-        try:
-            url = "https://www.virustotal.com/api/v3/files"
-            files = { "file": (file, open(file, "rb"), "text/plain") }
-            headers = {
-                    "accept": "application/json",
-                    "x-apikey": api_key}
-            
-            response = requests.post(url, files=files, headers=headers)
-            response.raise_for_status()  # Raise an exception if the response status code is not 200
-            return True
-        except requests.exceptions.HTTPError:
-            return False
-        
-    def Scan_id(self, file ,api_key):
-        url = "https://www.virustotal.com/api/v3/files"
-        files = { "file": (file, open(file, "rb"), "text/plain") }
-        headers = {
-                "accept": "application/json",
-                "x-apikey": api_key}
-            
-        response = requests.post(url, files=files, headers=headers)
-        
-        result = response.json()
-        scan_id = result.get('data', {}).get('id')
-        if scan_id:
-            return scan_id
-        else:
-            raise ValueError("Failed to retrieve scan ID from the response.")
+        def update_status(result, file):
+            status_text = f'{os.path.basename(file)}: {result}'
+            self.scan_status_var.set(status_text)
 
-    def check_file(self, file_path, api_key):
-        try:
-            Id = self.Scan_id(file_path, api_key)
-            url = f"https://www.virustotal.com/api/v3/analyses/{Id}"
-            headers = {
-                "accept": "application/json",
-                "x-apikey": api_key
-            }
-            response = requests.get(url, headers=headers)
+        def show_error(error_message):
+            self.scan_status_var.set(error_message)
 
-            # Process the response to check if the file is malicious
-            result = response.json()
-            
-            data = result["data"]
+        def scan_complete(message):
+            self.scan_status_var.set(message)
 
-            attributes = data["attributes"]
-
-            stats = attributes["stats"]
-
-            malicious_count = stats["malicious"]
-            
-            if malicious_count > 0:
-                return "Malicious"
-            else:
-                return "Clean"
-        except requests.exceptions.HTTPError as err:
-            print(f"HTTP Error: {err}")
-            return "Error"
-
-        except requests.exceptions.RequestException as err:
-            print(f"Request Exception: {err}")
-            return "Error"
-
-        except Exception as err:
-            print(f"Unexpected error occurred: {err}")
-            return "Error"
+        scan_thread = ScanThread(files, api_key, update_status, show_error, scan_complete)
+        scan_thread.start()
 
 
     def get_files_and_subdirectories(self, directory):
